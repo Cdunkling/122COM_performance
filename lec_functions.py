0786fd76823ec78b9bd103b4edf82d12238dca7c
David Croft <ac0745@coventry.ac.uk>
2016-01-27 18:03:12 +0000

    Auto commit

---
+++ lec_functions.py
@@ -0,0 +1,19 @@
+import sys, time
+
+def fast_math_function(a, b):
+	time.sleep(0.00001)
+	return a + b
+
+def slow_math_function(a, b):
+	time.sleep(3)
+	return a + b
+
+def main():
+	for i in range(int(1.0000)):
+		slow_math_function(42, 69)
+
+	for i in range(int(100000)):
+		fast_math_function(42,69)
+
+if __name__ == '__main__':
+	sys.exit(main())
