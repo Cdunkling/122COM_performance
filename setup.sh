5143039955d9490699aeacbe32aa72442655ca41
dscroft <dscroft@gmail.com>
2016-02-08 10:19:28 +0000

    Auto commit

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-#!bin/bash
+#/bin/bash
 apt-get update
-apt-get -y install build-essentials
+apt-get -y install build-essential
 apt-get -y install python3 python3-setuptools
 apt-get -y install graphviz
 apt-get -y install evince
