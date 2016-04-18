6cb14f49c20bf401358c46214429ffe9d13fba88
dcroft <ac0745@coventry.ac.uk>
2016-04-18 15:49:25 +0000

    Removed setup files

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-apt-get update
-apt-get -y install build-essential
-apt-get -y install python3 python3-setuptools
-apt-get -y install graphviz
-apt-get -y install evince
-easy_install3 snakeviz gprof2dot
