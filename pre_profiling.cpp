4d630e427b8a2a8aa653f2ed1058ba2b0f6b33db
dscroft <dscroft@gmail.com>
2016-05-24 11:44:46 +0100

    Auto commit

--- pre_profiling.cpp
+++ pre_profiling.cpp
@@ -47,35 +47,12 @@ vector<int> find_first_n_version2( int n )
     return primes;
 }
 
-vector<int> find_first_n_version3( int n )
-{
-    vector<int> primes;
-
-    int counter = 2;
-    while( primes.size() < n )
-    {
-        bool prime = true;
-
-        for( int i=0; prime && i<primes.size(); ++i )
-            if( counter % primes[i] == 0 )
-                prime = false;
-
-        if( prime )
-            primes.emplace_back(counter);
-
-        counter += 1;
-    }
-
-    return primes;
-}
-
 int main()
 {
     int primesToFind = 1000;
 
     find_first_n_version1( primesToFind );
     find_first_n_version2( primesToFind );
-    find_first_n_version3( primesToFind );
 
     return 0;
 }
